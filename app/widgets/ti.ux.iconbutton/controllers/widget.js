6a582d9072bb4720beaab990c8b1253a6754574f
Kosuke Isobe <naughtydog0721@gmail.com>
2014-06-05 21:48:42 +0900

    replace TiIconicFont on new version

--- app/widgets/ti.ux.iconbutton/controllers/widget.js
+++ app/widgets/ti.ux.iconbutton/controllers/widget.js
@@ -2,10 +2,10 @@
 var args = arguments[0];
 var WTools = require('WidgetTools');
 
-var fontawesome = require(WPATH('IconicFont')).IconicFont({
-	font: WPATH('FontAwesome'),
-	ligature: false	// optional
-});
+var IconicFont = require(WPATH('IconicFont')),
+	fontawesome = new IconicFont({
+		font: WPATH('FontAwesome')
+	});
 
 function initUI(){
 
@@ -18,7 +18,7 @@ function initUI(){
 
 //returns the whole map of charcodes
 $.getCharMap = function(){
-	return fontawesome.getCharMap() || {};
+	return fontawesome.font.charcode || {};
 };
 
 $.setIcon = function(codename){
@@ -29,7 +29,7 @@ $.init = function(argsInit){
 
 	$.btn.font = {
 		fontSize: args.size || 24,
-		fontFamily: fontawesome.fontfamily()
+		fontFamily: fontawesome.fontfamily
 	};
 
 	if(argsInit.iconColor) $.btn.color = args.iconColor;
